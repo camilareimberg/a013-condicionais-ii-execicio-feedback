5ddf1a2498696e0e51127998993cc62b9f5af7cf
camilareimberg <camila_reimberg@hotmail.com>
2022-08-08 21:58:40 -0300

    Camila Reimberg - exercicio.feedback-aula13

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,29 @@
 const num = //seu numero aqui
+// feito durante a aula
 
+console.log('Prática guiada operador lógico')
+const numero= Number(prompt('Digite um número'))
+
+// utilizando operador lógico
+if (numero%2===0 && numero%3===0) {
+console.log('\n O número É divisível por 2 e por 3');
+
+} else {
+    console.log('\n O número NÃO é divisível por 2 e por 3');
+}
+
+// utilizando if aninhado
+console.log('\n \n Prática guiada if aninhado')
+if (numero%2===0) {
+    if (numero%3===0) {
+        console.log('O número É divisível por 2 e por 3');
+    } else {
+        console.log('O número NÃO é divisível por 2 e por 3');
+    }
+    } else {console.log('O número NÃO é divisível por 2 e por 3');
+}
+
+//usando o if ternário
+
+console.log('\n \n Prática guiada - if ternário')
+console.log(numero===30? 'Ótima opção, 30 é divisível por 2 e por 3' : 'Não foi digitado 30')
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,9 @@
-//código a ser reescrito
+//código a ser reescrito com o if ternário
 
+nome = prompt("Digite o sue nome")
+idade = Number(prompt("Digite a sua idade"))
+
+console.log('Utilizando o if')
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
@@ -11,3 +15,10 @@ if(idade >= 18){
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+
+// resolução com o if ternário
+
+console.log('\n \n If ternário')
+console.log(nome==="José"? 'Oi, Zé!' : `Olá ${nome}`)
+
+console.log(idade>=18? 'Você já pode tirar carteira de motorista!' : 'Aguardar mais um pouco. Você ainda não pode tirar carteira de motorista')
